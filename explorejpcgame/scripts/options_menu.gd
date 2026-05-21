5736e523e54cae105c9770e5a6739e9cc697537e
All-an-Hu-ang <20050@jpc.school.nz>
2026-05-21 12:38:15 +1200

    Options finished

--- explorejpcgame/scripts/options_menu.gd
+++ explorejpcgame/scripts/options_menu.gd
@@ -1,6 +1,8 @@
 extends Control
 
-
+func _ready() -> void:
+	$"margin container/side panel h box/side panel v box/language panel/checkbox h box/checkbox v box/english".button_pressed = true
+	$"margin container/panel h box/panel/options panel/choice v box/choice h box/checkbox h box/option 1".button_pressed = true
 
 func _on_resume_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
@@ -12,12 +14,3 @@ func _on_sound_button_pressed() -> void:
 
 func _on_display_button_pressed() -> void:
 	$"margin container/panel h box/panel/title".text = "Display"
-
-
-func _on_option_1_toggled(toggled_on: bool) -> void:
-	$"margin container/panel h box/panel/options panel/choice v box/choice h box/checkbox h box/option 2".button_pressed = false
-
-
-
-func _on_option_2_toggled(toggled_on: bool) -> void:
-	$"margin container/panel h box/panel/options panel/choice v box/choice h box/checkbox h box/option 1".button_pressed = false
