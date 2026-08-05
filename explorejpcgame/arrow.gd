c1e57a61fd79b630d1f6c9f836ceea17357c4742
All-an-Hu-ang <20050@jpc.school.nz>
2026-08-05 12:07:14 +1200

    Arrow completed

---
+++ explorejpcgame/arrow.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	print(global_position.y)
+	if ObjectiveManager.current_objective:
+		look_at(ObjectiveManager.current_objective.global_position, Vector3.UP)
